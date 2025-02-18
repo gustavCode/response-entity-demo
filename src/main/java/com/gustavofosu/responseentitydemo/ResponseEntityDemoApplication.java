57dc4aba014b70f5172b00ca723d89814b7ab389
Gustav Ofosu <gustavofosu@gmail.com>
2025-02-18 11:07:36 +0000

    Initial commit

---
+++ src/main/java/com/gustavofosu/responseentitydemo/ResponseEntityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gustavofosu.responseentitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseEntityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResponseEntityDemoApplication.class, args);
+    }
+
+}
